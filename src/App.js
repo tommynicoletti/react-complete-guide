83b728ce963c3b547eaba757f92d7e684c49e5eb
unknown <masinonicoletti@gmail.com>
2020-03-01 20:24:16 +0100

    Props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working</p>
-      <Person />
+      <Person name="Max" age="28" />
+      <Person name="Manu" age="29">
+        My Hobbies: Coding
+      </Person>
+      <Person name="Stephanie" age="26" />
     </div>
   );
   // return React.createElement('div', { className: 'App'}, React.createElement( 'h1', null, 'Does this work now?'));
