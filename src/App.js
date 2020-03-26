b795e038e5cf91937441d72abdd9833f3c227960
Tommaso <masinonicoletti@gmail.com>
2020-03-26 22:46:58 +0100

    From functional component to class component Click handler

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working</p>
-      <Person name="Max" age="28" />
-      <Person name="Manu" age="29">
-        My Hobbies: Coding
-      </Person>
-      <Person name="Stephanie" age="26" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 }
+    ],
+    otherState: "Some other value"
+  }
+
+  switchNameHandler = () => {
+    // DON'T USE 'this' KEYWORD
+    this.setState({
+      persons: [
+        { name: "Maximilian", age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working</p>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    )
+  }
   // return React.createElement('div', { className: 'App'}, React.createElement( 'h1', null, 'Does this work now?'));
 }
 
